9ffd37dc4d3dd62601745001b1e35ce60887c694
Khalatska <135690966+Khalatska@users.noreply.github.com>
2024-03-26 11:02:41 +0100

    added js to menu

--- main.js
+++ main.js
@@ -8,3 +8,23 @@ btnOrder.addEventListener("click", () => {
 btnClose.addEventListener("click", () => {
   modalElement.classList.remove("is-open");
 });
+
+const mobMenuElement = document.querySelector(".mob-menu");
+const btnOpen = document.querySelector(".menu-open-btn");
+const btnCloseMenu = document.querySelector(".menu-close-btn");
+
+btnOpen.addEventListener("click", () => {
+  mobMenuElement.classList.add("is-open");
+  if (window.innerWidth < 768) {
+    btnCloseMenu.style.display = "block";
+    btnOpen.style.display = "none";
+  }
+});
+
+btnCloseMenu.addEventListener("click", () => {
+  mobMenuElement.classList.remove("is-open");
+  if (window.innerWidth < 768) {
+    btnOpen.style.display = "block";
+    btnCloseMenu.style.display = "none";
+  }
+});
